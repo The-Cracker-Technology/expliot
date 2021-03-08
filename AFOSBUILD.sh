710cd6dfb5356a229962d004387490e63338ff83
TCT Source Code TEAM <source@thecrackertechnology.com>
2021-03-08 12:45:30 -0300

    Update AFOSBUILD.sh

--- AFOSBUILD.sh
+++ AFOSBUILD.sh
@@ -1,6 +1,8 @@
 rm -rf /opt/ANDRAX/bin/expliot
 cp -Rf andraxbin/* /opt/ANDRAX/bin
 
+chmod -R 755 /opt/ANDRAX/bin
+
 export PYTHONPATH=/opt/ANDRAX/expliot/lib/python3.7/site-packages/
 
 python3 setup.py install --prefix=/opt/ANDRAX/expliot
